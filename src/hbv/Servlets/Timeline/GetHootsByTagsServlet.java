e732bbafbd6047f0a3a94b2b8bd90ba83f1365cb
Ulf <ulftietze@icloud.com>
2022-02-17 19:06:14 +0100

    Create servlets for the corresponding api json

---
+++ src/hbv/Servlets/Timeline/GetHootsByTagsServlet.java
@@ -0,0 +1,29 @@
+package hbv.Servlets.Timeline;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/getHootsByTags")
+public class GetHootsByTagsServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<!doctype html><html>");
+        out.println("<head> <meta charset='utf-8'>");
+        out.println("<title>webapp</title> </head>");
+        out.println("<body>GetHootsByTagsServlet</body>");
+        out.println("</html>");
+
+        ServletContext context = getServletContext();
+        context.log("simple logging");
+    }
+}
