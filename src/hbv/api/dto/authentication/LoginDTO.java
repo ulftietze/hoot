a81e8606a7ae14c1897dd17394ed852983a85020
Fellow student CV <no-reply@studenten.hs-bremerhaven.de>
2022-02-19 16:46:02 +0100

    Java API DTO Classes

---
+++ src/hbv/api/dto/authentication/LoginDTO.java
@@ -0,0 +1,8 @@
+package hbv.api.dto.authentication;
+
+public class LoginDTO
+{
+    public String userName;
+
+    public String password;
+}
---
+++ src/hbv/api/dto/authentication/RegisterDTO.java
@@ -0,0 +1,12 @@
+package hbv.api.dto.authentication;
+
+public class RegisterDTO
+{
+    public String userName;
+
+    public String password;
+
+    public String imageFilename;
+
+    public String image;
+}
