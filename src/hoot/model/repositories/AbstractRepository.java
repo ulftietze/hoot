1b142d8883202fd71f132ba6516d5b82606b4ca2
Fellow student CV <no-reply@studenten.hs-bremerhaven.de>
2022-02-22 12:59:26 +0100

    revert DatabaseConnectionPool - Tomcat already pools connections

--- src/hoot/model/repositories/AbstractRepository.java
+++ src/hoot/model/repositories/AbstractRepository.java
@@ -1,40 +1,15 @@
 package hoot.model.repositories;
 
 import hoot.model.search.SearchCriteriaInterface;
-import hoot.system.Database.DatabaseConnectionPool;
 import hoot.system.Exception.CouldNotDeleteException;
 import hoot.system.Exception.CouldNotSaveException;
 import hoot.system.Exception.EntityNotFoundException;
 import hoot.system.Exception.ConnectionFailedException;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 
 public abstract class AbstractRepository<Type>
 {
-    private final DatabaseConnectionPool pool;
-
-    public AbstractRepository()
-    {
-        this.pool = DatabaseConnectionPool.getPool();
-    }
-
-    protected Connection getConnection() throws ConnectionFailedException
-    {
-        if (this.pool == null) {
-            throw new ConnectionFailedException();
-        }
-        return pool.get();
-    }
-
-    protected void returnConnection(Connection connection) throws ConnectionFailedException
-    {
-        if (this.pool == null) {
-            throw new ConnectionFailedException();
-        }
-        pool.put(connection);
-    }
-
     public abstract Type getById(int id) throws EntityNotFoundException, ConnectionFailedException;
 
     public abstract ArrayList<Type> getList(SearchCriteriaInterface searchCriteria);
--- src/hoot/model/repositories/UserRepository.java
+++ src/hoot/model/repositories/UserRepository.java
@@ -8,11 +8,17 @@ import hoot.system.Exception.CouldNotDeleteException;
 import hoot.system.Exception.CouldNotSaveException;
 import hoot.system.Exception.EntityNotFoundException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class UserRepository extends AbstractRepository<UserDTO>
 {
@@ -20,7 +26,10 @@ public class UserRepository extends AbstractRepository<UserDTO>
     public UserDTO getById(int id) throws EntityNotFoundException, ConnectionFailedException
     {
         try {
-            Connection connection = this.getConnection();
+            Context    initCtx    = new InitialContext();
+            Context    envCtx     = (Context) initCtx.lookup("java:/comp/env");
+            DataSource ds         = (DataSource) envCtx.lookup("jdbc/mariadb");
+            Connection connection = ds.getConnection();
 
             PreparedStatement pss = connection.prepareStatement("select id, username, imagePath from User where id = ?");
             pss.setInt(1, id);
@@ -40,10 +49,10 @@ public class UserRepository extends AbstractRepository<UserDTO>
             rs.close();
             pss.close();
 
-            this.returnConnection(connection);
+            connection.close();
 
             return user;
-        } catch (SQLException e) {
+        } catch (SQLException | NamingException e) {
             throw new EntityNotFoundException("User");
         }
     }
