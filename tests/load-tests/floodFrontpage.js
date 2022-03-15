2da0e6b057421eea7d9e31175af66aae894a86e9
Ulf <ulftietze@icloud.com>
2022-03-15 11:45:45 +0100

    New Script

--- tests/load-tests/floodFrontpage.js
+++ tests/load-tests/floodFrontpage.js
@@ -3,8 +3,8 @@ import { check } from 'k6';
 
 export let options = {
     vus: '50',
-    //duration: '120s',
-    iterations    : '300000',
+    duration: '180s',
+    //iterations    : '300000',
     noCookiesReset: true,
 };
 
